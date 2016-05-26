f0f6a70e7e92de9c92be5d086ddd5dd9ec8a0fe2
denis999943 <vuyahevich.1996@gmail.com>
2016-05-26 17:22:46 +0300

    26.05.2016

---
+++ www/checkin.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Sanatana HTML Version - Contacts</title>
+<meta name="description" content="Default Description" />
+<meta name="keywords" content="Santana, Varien, E-commerce" />
+<meta name="robots" content="INDEX,FOLLOW" />
+<link rel="icon" href="#" type="image/x-icon" />
+<link rel="shortcut icon" href="#" type="image/x-icon" />
+
+<!-- CSS =====================================================================================-->
+<link href='http://fonts.googleapis.com/css?family=Telex' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" type="text/css" href="css/styles.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/skin.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/cloud-zoom.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/light_box.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/mix.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/banner.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/magicat.css" media="all" />
+
+<!-- Scripts =====================================================================================-->
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
+<script type="text/javascript" src="js/menu.js"></script>
+<script type="text/javascript" src="js/banner_pack.js"></script>
+<script type="text/javascript" src="js/light_box.js"></script>
+<script type="text/javascript" src="js/cloud-zoom.1.0.2.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+<script type="text/javascript" src="js/jquery.mix.js"></script>
+</head>
+<body id="bg_color" class=" cms-index-index cms-home">
+<!--START OF WRAPPER-->
+<div class="wrapper">
+  <div class="page"> 
+    
+    <!--START OF HEADER-->
+    <div class="header-container">
+      <div class="quick-access"> 
+        <!--Start Block Cart-->
+        <div class="block block-cart header_cart">
+          <div class="block-content_pan">
+            <div class="summary trigger-minicart">
+              <h2 class="classy"> <span class="cart_icon"><img alt="" src="images/shoppingbag.png" /></span><a href="calc.html">Кошик</a> </h2>
+            </div>
+            <div class="remain_cart" id="minicart">
+              <p class="empty">У вас немає товарів у кошику.</p>
+              <div class="actions">
+                <p class="subtotal"> <span class="label">До сплати:</span> <span class="price">0.00 грн.</span> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--End Block Cart--> 
+        
+       <!--Start Toplinks-->
+        <ul class="links">
+          <li class="first"><a href="http://localhost/login.php" title="My Account">Увійти</a></li>
+          <li class=" last"><a href="http://localhost/checkin.php" title="Log In">Реєстрація</a></li>
+		  <li class=" last"><a href="http://localhost/pass.php">Змінити пароль</a></li>
+
+        </ul>
+        <!--End Toplinks--> 
+        
+        
+      </div>
+      <!--Start Header Content-->
+      <div class="header">
+        <ul id="logo">
+          <!--Left-->
+          <li class="head-container"> <span>|</span>
+            <h2 class="classy">Безкоштовна доставка по всій Україні при замовлені на суму понад 5000 грн.</h2>
+            <span>|</span>
+            <p class="top-welcome-msg"></p>
+          </li>
+          <!--Left--> 
+          <!--Center Logo-->
+          <li class="logo-box">
+            <h1 class="logo"><strong>Magento Commerce</strong><a href="category.html" title="Magento Commerce" class="logo"><img src="images/logo.png" alt="Magento Commerce" /></a></h1>
+          </li>
+          <!--Center Logo--> 
+          
+          <!--Right-->
+          <li class="head-container"> <span>|</span>
+            <h2 class="classy">Телефон для довідок 095-91-44-375</h2>
+            <span>|</span>
+            <div id="search-bar">
+              <div class="top-bar">
+                <form id="search_mini_form" action="">
+                  <div class="form-search">
+                    <input
+                     onfocus="if(this.value=='Пошук') {this.value=''};" onblur="if(this.value=='') {this.value='Search'};"
+                     id="search" name="q" value="Пошук" class="input-text" type="text" />
+                    <button type="submit" title="Пошук" class="button">Пошук</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </li>
+          <!--Right-->
+        </ul>
+        <!--Start of Navigation-->
+        <div class="nav-container">
+          <ul id="nav">
+            <li><a href="category.html">Головна</a></li>
+            <li class="level0 nav-1 level-top first parent"> <a href="windows.html" class="level-top"> <span>Windows</span> </a>
+              <ul class="level0">
+                <li class="level1 first parent"> <a href="#"> <span>Бренд</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>Microsoft</span> </a> </li>
+					<li class="level2"> <a href="#"> <span>HTC</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Prestigio</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>ZTE</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>Acer</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Діагональ екрана</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>4"</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4"-4,7"</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4,7"-5"</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 5"</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Колір</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>Чорний</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Білий</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Золотий</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Срібло</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>Жовтий</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Об’єм RAM</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>< 1 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>1-2 gb</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 2 gb</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 last parent"> <a href="#"> <span>Об’єм пам’яті</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>4 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4-8 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>8-16 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>16-32 gb</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 32 gb</span> </a> </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li class="level0 nav-1 level-top first parent"> <a href="#" class="level-top"> <span>IOS</span> </a>
+              <ul class="level0">
+                <li class="level1 first parent"> <a href="#"> <span>Бренд</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>Apple</span> </a> </li>
+					
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Діагональ екрана</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>4"</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4,7"</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>5,5"</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Колір</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>Чорний</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Білий</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Золотий</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Срібло</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>Рожевий</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Об’єм RAM</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>< 1 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>1-2 gb</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 2 gb</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 last parent"> <a href="#"> <span>Об’єм пам’яті</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>16 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>32 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>64 gb</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>128 gb</span> </a> </li>
+                  </ul>
+                </li>
+              </ul>
+			  <li class="level0 nav-1 level-top first parent"> <a href="#" class="level-top"> <span>Android</span> </a>
+              <ul class="level0">
+                <li class="level1 first parent"> <a href="#"> <span>Бренд</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>Microsoft</span> </a> </li>
+					<li class="level2"> <a href="#"> <span>HTC</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Samsung</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Lenovo</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>інші</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Діагональ екрана</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>4"</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4"-4,7"</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4,7"-5"</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 5"</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Колір</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>Чорний</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Білий</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Золотий</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>Срібло</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>інший</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 parent"> <a href="#"> <span>Об’єм RAM</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>< 1 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>1-2 gb</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 2 gb</span> </a> </li>
+                  </ul>
+                </li>
+                <li class="level1 last parent"> <a href="#"> <span>Об’єм пам’яті</span> </a>
+                  <ul class="level1">
+                    <li class="level2 first"> <a href="#"> <span>4 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>4-8 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>8-16 gb</span> </a> </li>
+                    <li class="level2"> <a href="#"> <span>16-32 gb</span> </a> </li>
+                    <li class="level2 last"> <a href="#"> <span>> 32 gb</span> </a> </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+			
+			
+			
+            </li>
+            <li class="level0 level-top"> <a href="index2.html" class="level-top"> <span>Телефони без ОС</span> </a> </li>
+			<li class="level0 level-top"> <a href="index.html" class="level-top"> <span>Всі телефони</span> </a> </li>
+ </li>
+          </ul>
+        </div>
+        <!--End of Navigation--> 
+      </div>
+      <!--End Header Content--> 
+    </div>
+    <!--END OF HEADER--> 
+    
+    <!--START OF MAIN CONTENT-->
+    <div class="main-container col2-left-layout">
+      <div class="main">
+        <div class="col-main">
+          <div id="messages_product_view"></div>
+          <!--Page Title-->
+          <div class="page-title">
+            <h1>Реєстрація</h1>
+          </div>
+		  
+          		<!--Start of Contact Form-->
+				<!--Для шифрування паролю потрібно в рядок 282, враховуючи що цей знаходиться на 265, вставити наступне: $password=md5($password);-->
+              <div class="fieldset">
+              <h2 class="legend">Контактна інформація</h2>	
+			
+			<?php
+			
+			$connect = mysql_connect('localhost','denis999943','denis1996') or die(mysql_error());
+			mysql_select_db('denis999943');
+			
+			if(isset($_POST['submit'])){
+			    $name=$_POST['name'];
+			    $sername=$_POST['sername'];
+			    $email=$_POST['email'];
+			    $login=$_POST['login'];
+			    $password=$_POST['password'];
+			    $r_password=$_POST['r_password'];
+			    if ($password==$r_password){
+				    
+				    echo "Ви успішно зареєстровані! Тепер ви можете зайти на <a href='category.html'>головну сторінку</a>";
+			    } 
+			    else{
+				    die('Паролі не співпадають!!!');
+			    }
+			    $query=mysql_query("INSERT INTO client VALUES ('','$name','$sername','$email','$login','$password')") or die(mysql_error());
+			} else {
+			?>
+			
+	
+			<form action = "checkin.php" method="post">
+		    <p><h2 class="classy">Ім’я</h2><br><input type="text" name="name" required/></p>
+			<p><h2 class="classy">Прізвище</h2><br><input type="text" name="sername" required/></p>
+			<p><h2 class="classy">Дата народження</h2><br><input type="date" name="calendar" /></p>
+			<p><h2 class="classy">Email</h2><br><input type="text" name="email" /></p>
+			<p><h2 class="classy">Логін</h2><br><input type="text" name="login" required/></p>
+			<p><h2 class="classy">Пароль</h2><br><input type="password" name="password" required/></p>
+			<p><h2 class="classy">Повторіть пароль</h2><br><input type="password" name="r_password" required/></p>
+			<input class="fancybox quickllook" type = "submit" name="submit" value="Отправить">
+			</form>
+			<?php } ?>
+			
+			
+          </form>
+          <!--End of Contact Form-->  
+		  
+        </div>
+		
+		
+        <div class="col-left sidebar"> 
+        
+          
+          
+        </div>
+      </div>
+      <div style="display: none;" id="back-top"> <a href="#"><img alt="" src="images/backtop.gif" /></a> </div>
+    </div>
+    <!--END OF MAIN CONTENT--> 
+    
+    <!--START OF FOOTER-->
+    <div class="footer-container">
+      <div class="footer">
+        <div class="f-fix">
+          <div class="frame">.</div>
+          <!--Shipping Block-->
+          <div class="free-shipping">Безкоштовна доставка для Вас <span>гарантовано до кожного замовлення подарунок!</span></div>
+          <!--Shipping Block--> 
+          
+          <!--Newsletter-->
+          <form method="post" id="newsletter-validate-detail" action="">
+            <div class="form-subscribe">
+              <div class="form-subscribe-header">Підписатися на наші новини</div>
+              <div class="input-box">
+                <input 
+                onfocus="if(this.value=='Введіть свою адресу') {this.value=''};" onblur="if(this.value=='') {this.value='Введіть свою адресу'};"
+                value="Введіть свою адресу" name="email" id="newsletter" title="Sign up for newsletter" class="input-text required-entry validate-email" type="text" />
+                <button type="submit" title="Submit" class="button"><span>Надіслати</span></button>
+              </div>
+            </div>
+          </form>
+          <!--Newsletter--> 
+       
+            </div>
+            <div class="block_2">
+              <h3>Ми в </h3>
+              <!--Social links-->
+              <div class="social_box">
+                <div class="facebook"><a title="Facebook" href="http://vk.com/login?act=mobile&hash=c965156c62cd01d0"></a></div>
+                <div class="twitter"><a title="Twitter" href="http://vk.com/login?act=mobile&hash=c965156c62cd01d0"></a></div>
+                <div class="linkedin"><a title="Linkedin" href="http://vk.com/login?act=mobile&hash=c965156c62cd01d0"></a></div>
+                <div class="youtube"><a title="Youtube" href="http://vk.com/login?act=mobile&hash=c965156c62cd01d0"></a></div>
+              </div>
+              <!--Payment Icons-->
+              <div class="payment"> <img src="images/amexp.jpg" alt="" /> <img src="images/visa.jpg" alt="" /> <img src="images/mastercard.jpg" alt="" /> <img src="images/discover.jpg" alt="" /> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <address>
+      © 2016 На Зв’язку. Вуяхевич Денис
+      </address>
+    </div>
+    <!--END OF FOOTER--> 
+  </div>
+ <script type="text/javascript">
+	 var minicart_timer;
+    jQuery(".trigger-minicart").hover(function(){
+        jQuery("#minicart").slideDown();
+    });
+    jQuery("#minicart").mouseleave(function(){
+        jQuery("#minicart").slideUp();
+    });
+    jQuery("#minicart").hover(function(){
+        clearTimeout(minicart_timer);
+    });
+	jQuery(document).ready(function(){
+	jQuery('.pagebox_btn').click(function(){
+		if(parseInt(jQuery('.page_pan').css('left')) < 0)
+		{
+			jQuery('.page_pan').animate({ left: '0' }, 600, 'easeOutQuint');
+		}
+		else{
+			jQuery('.page_pan').animate({ left: '-100px' }, 600, 'easeOutQuint');
+			}
+		});
+		});
+</script>
+</div>
+
+<!--END OF WRAPPER-->
+</body>
+</html>
